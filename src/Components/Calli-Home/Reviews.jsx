33a8cb8583f2928bc0d0f83ede4887fe2234efc9
Afrimann <omotoshopeter737@gmail.com>
2024-06-04 14:43:22 +0100

    Added review hyeading

--- src/Components/Calli-Home/Reviews.jsx
+++ src/Components/Calli-Home/Reviews.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import '../Calli-Home/Reviews.css'
 import reviewImage1 from '../Images/calligraphy-artist-reviewer.jpg'
 import reviewImage2 from '../Images/calligraphy-artist-reviewer-2.jpg'
@@ -7,14 +7,14 @@ import Sponsors from './Sponsors'
 const Reviews = () => {
     const reviewDetail = [
         {
-            name :'EMMA HART',
-            picture : reviewImage1,
-            content : '“Massa amet, at dolor tellus pellentesque aenean in eget massa tincidunt habitasse volutpat adipiscing sed id sit auctor eu vivamus nulla.”'
+            name: 'EMMA HART',
+            picture: reviewImage1,
+            content: '“Massa amet, at dolor tellus pellentesque aenean in eget massa tincidunt habitasse volutpat adipiscing sed id sit auctor eu vivamus nulla.”'
         },
         {
-            name :'ERIK DOE',
-            picture : reviewImage2,
-            content : '“Massa amet, at dolor tellus pellentesque aenean in eget massa tincidunt habitasse volutpat adipiscing sed id sit auctor eu vivamus nulla.”'
+            name: 'ERIK DOE',
+            picture: reviewImage2,
+            content: '“Massa amet, at dolor tellus pellentesque aenean in eget massa tincidunt habitasse volutpat adipiscing sed id sit auctor eu vivamus nulla.”'
         },
     ]
     const transition = [{ type: 'spring', duration: 3 }]
@@ -35,33 +35,36 @@ const Reviews = () => {
         visible: { opacity: 1, y: 0 }
     }
 
-  return (
-    <div className='Reviews'>
-        <motion.div 
-        variants={parentVariants}
-        transition={{duration : 0.5}}
+    return (
+        <div className='Reviews'>
+            <h2 style={{paddingBottom: '30px'}}>My Reviews</h2>
 
-        className="review-container">
-            {
-                reviewDetail.map((review,index)=>(
-                    <motion.div 
-                    variants={spanVariants}
-                    initial = "hidden"
-                    whileInView= "visible"
-                    transition={{duration : 0.5}}
+            <motion.div
+                variants={parentVariants}
+                transition={{ duration: 0.5 }}
+
+                className="review-container">
+
+                {
+                    reviewDetail.map((review, index) => (
+                        <motion.div
+                            variants={spanVariants}
+                            initial="hidden"
+                            whileInView="visible"
+                            transition={{ duration: 0.5 }}
 
 
-                    className="review">
-                        <img src={review.picture} alt="" />
-                        <p>{review.content}</p>
-                        <span>{review.name}</span>
-                    </motion.div>
-                ))
-            }
-        </motion.div>
+                            className="review">
+                            <img src={review.picture} alt="" />
+                            <p>{review.content}</p>
+                            <span>{review.name}</span>
+                        </motion.div>
+                    ))
+                }
+            </motion.div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Reviews
